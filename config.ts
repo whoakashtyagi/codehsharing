cdc89550fa720f83b7a4f566078743f709bb01ab
Akash Tyagi <62569110+whoakashtyagi@users.noreply.github.com>
2022-02-15 15:16:37 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -3,11 +3,10 @@ const config = {
         cert: './source/config/saml.pem',
         entryPoint: 'https://akash-dev.onelogin.com/trust/saml2/http-post/sso/23312094-037a-459f-a235-223ded17218a',
         issuer: 'http://localhost:1337',
-        // forceAuthn: true,
         options: {
             failureRedirect: '/login',
             failureFlash: true,
-            forceAuthn: true
+            
         }
     },
     server: {
@@ -16,7 +15,8 @@ const config = {
     session: {
         resave: true,
         secret: 'supersecretamazingpassword',
-        saveUninitialized: true
+        saveUninitialized: true,
+        maxTimeSinceAuthn: 30
     }
 };
 
